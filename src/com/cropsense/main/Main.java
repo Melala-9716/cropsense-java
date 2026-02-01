3c88a25a3848bef3bf30bb348cf92593bac85c8f
muaz-4039 <muaz.ugr-4766-17@aau.edu.et>
2026-02-01 19:48:36 +0300

    Update Main.java

--- src/com/cropsense/main/Main.java
+++ src/com/cropsense/main/Main.java
@@ -1,14 +1,16 @@
 package com.cropsense.main;
 
-import com.cropsense.gui.view.LoginView;
+import com.cropsense.gui.view.RoleSelectionView;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        new LoginView().show(stage);
+        stage.getIcons().add(new Image("wheat.png"));
+        new RoleSelectionView().show(stage);
     }
 
     public static void main(String[] args) {
