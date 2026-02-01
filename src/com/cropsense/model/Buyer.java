95372d217e49e46bc9840b99d580327b38f73bd1
melala muleta <melalamuleta54@gmail.com>
2026-02-01 14:58:55 +0300

    Update Buyer.java

--- src/com/cropsense/model/Buyer.java
+++ src/com/cropsense/model/Buyer.java
@@ -1,8 +1,10 @@
 package com.cropsense.model;
 
+// Buyer class extends User and represents a user with the role "BUYER"
 public class Buyer extends User {
-    public Buyer (String id , String name , String role){
-
-        super(id,name, "BUYER");
+    public Buyer(String id, String name, String role) {
+        // Calls the User constructor with id, name, and fixed role "BUYER"
+        super(id, name, "BUYER");
     }
 }
+
