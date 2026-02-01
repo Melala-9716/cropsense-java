6e302cec587f361e1573a10466c6497f202120a2
melala muleta <melalamuleta54@gmail.com>
2026-02-01 14:59:50 +0300

    Update Farmer.java

--- src/com/cropsense/model/Farmer.java
+++ src/com/cropsense/model/Farmer.java
@@ -1,8 +1,10 @@
 package com.cropsense.model;
 
+// Farmer class extends User and represents a user with the role "FARMER"
 public class Farmer extends User {
-    public Farmer(String id , String name, String role){
-        super(id,name , "FARMER");
+    public Farmer(String id, String name, String role) {
+        // Calls the User constructor with id, name, and fixed role "FARMER"
+        super(id, name, "FARMER");
     }
-    
 }
+
